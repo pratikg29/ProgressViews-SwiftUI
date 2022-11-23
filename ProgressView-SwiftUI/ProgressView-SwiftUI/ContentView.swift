aa21a502a1407db6212082667677dd3651317613
Pratik Gadhesariya <pratikgadhesariya29@gmail.com>
2022-11-23 20:24:30 +0530

    Added blank project

---
+++ ProgressView-SwiftUI/ProgressView-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProgressView-SwiftUI
+//
+//  Created by Pratik on 23/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProgressView-SwiftUI/ProgressView-SwiftUI/ProgressView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressView_SwiftUIApp.swift
+//  ProgressView-SwiftUI
+//
+//  Created by Pratik on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProgressView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
